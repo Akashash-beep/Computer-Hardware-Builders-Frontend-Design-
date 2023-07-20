c2458acfc0d33593ed004a3167ab7c61dab78fc3
Akash Ragupathy <57954503+Akashash-beep@users.noreply.github.com>
2023-07-20 07:04:48 +0100

    upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let image = document.getElementById('image');
+let path = ['Assets/MacBook.png', 'Assets/iMac.png', 'Assets/Processor.png'];
+let hamburger = document.getElementById('ham');
+let navbar = document.getElementById('nav');
+let x = navbar.querySelectorAll('a');
+
+hamburger.addEventListener('click', () => {
+
+    navbar.classList.toggle('activated');
+
+
+})
+for (let i = 0; i < x.length; i++) {
+    x[i].addEventListener('click', () => {
+        navbar.classList.toggle('activated');
+    })
+}
+console.log(x);
+// let currentIndex = 0;
+// function changebg() {
+
+//     image.src = path[currentIndex];
+//     currentIndex = (currentIndex + 1) % path.length;
+//     console.log(currentIndex);
+// }
+// setInterval(changebg, 3000);
